28753dcf0c733ca859c82626a0acd1fdf62d5588
shreyanshs7 <shreyanshss7@gmail.com>
2018-12-23 23:25:25 +0530

    Added Js changes for profile edit

--- routes/users.js
+++ routes/users.js
@@ -3,84 +3,104 @@ var User = require('../models/user');
 var userStat = require('../models/statistics');
 var router = express.Router();
 
-router.post('/register', function(req, res) {
+router.post('/register', function (req, res) {
   var name = req.body.name;
   var username = req.body.username;
   var password = req.body.password;
 
   var user = new User({
-    name : name,
-    username : username,
-    password : password
+    name: name,
+    username: username,
+    password: password
   });
-  user.save(function(err){
-    if(err) throw err;
+  user.save(function (err) {
+    if (err) throw err;
   });
-  User.findOne({ username : username} ).exec()
-  .then(response => {
-    var newUserStat = new userStat({
-      user : username,
+  User.findOne({ username: username }).exec()
+    .then(response => {
+      var newUserStat = new userStat({
+        user: username,
+      });
+      newUserStat.save((err) => {
+        if (err) throw err;
+      });
+    })
+    .catch(error => {
+      console.log(error);
     });
-    newUserStat.save((err) => {
-      if(err) throw err;
-    });
-  })
-  .catch(error => {
-    console.log(error);
-  });
-  res.json({ success : true, message : "User registered" });
+  res.json({ success: true, message: "User registered" });
 });
 
-router.post('/login', function(req, res){
+router.post('/login', function (req, res) {
   var username = req.body.username;
   var password = req.body.password;
-  User.findOne({ username : username }).exec().then(user => {
-    if(!user) {
-      res.json({ success : false, message : "No user with username found" });
+  User.findOne({ username: username }).exec().then(user => {
+    if (!user) {
+      res.json({ success: false, message: "No user with username found" });
     }
-    if(!user.validatePassword(password, user.password)) {
-      res.json({ success : false, message : "Invalid password" });
+    if (!user.validatePassword(password, user.password)) {
+      res.json({ success: false, message: "Invalid password" });
     }
-    else{
+    else {
       var session = req.session;
       session.username = user.username;
-      res.json({ success : true, message : "User authenticated"});
+      res.json({ success: true, message: "User authenticated" });
     }
   })
-  .catch(err => {
-    console.log("ERROR ", err);
-  });
+    .catch(err => {
+      console.log("ERROR ", err);
+    });
 });
 
-router.get('/logout', function(req, res){
-  req.session.destroy(function(err){
-    if(err) throw err;
+router.get('/logout', function (req, res) {
+  req.session.destroy(function (err) {
+    if (err) throw err;
     else res.redirect('/');
   });
 });
 
-router.get('/login', function(req, res){
-  if(req.session.username) {
-    res.render('profile');
+router.get('/login', function (req, res) {
+  if (req.session.username) {
+    User.findOne({ username: req.session.username }).exec()
+      .then(user => {
+        userStat.findOne({ user: username }).exec()
+          .then(stats => {
+            res.render('profile', { user: user, stats: stats });
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
   else
     res.render('login');
 });
 
-router.get('/register', function(req, res){
+router.get('/register', function (req, res) {
   res.render('register');
 });
 
-router.get('/profile', function(req, res){
-  if(req.session.username)
-    res.render('profile');
+router.get('/profile', function (req, res) {
+  if (req.session.username) {
+    User.findOne({ username: req.session.username }).exec()
+      .then(user => {
+        userStat.findOne({ user : req.session.username }).exec()
+          .then(stats => {
+            res.render('profile', { user: user, stats: stats });
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
   else
     res.render('login');
-});
-
-
-router.get('/profile/details', function(req, res){
-
 });
 
 module.exports = router;
